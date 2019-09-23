8986853e522ffbe5af255e96628585ad75b8a4d0
eldar.bajrovic <eldar.bajrovic@gmail.com>
2019-09-23 11:18:04 +0200

    Dodan property u ZajednickaKlasaVM

--- EFCore2017/ViewModels/ZajednickaKlasaVM.cs
+++ EFCore2017/ViewModels/ZajednickaKlasaVM.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EFCore2017.EntityModels;
 
 namespace EFCore2017.ViewModels
 {
     public class ZajednickaKlasaVM
     {
+        public List<Igra> IgreList { get; set; }
+
+        public List<Korisnik> KorisniciList { get; set; }
     }
 }
